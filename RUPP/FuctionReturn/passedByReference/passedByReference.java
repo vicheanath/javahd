e2c07b59e4da1b50f1fb0e92fa4cc1988b5d1d58
Vichea Nath <48352653+vicheanath@users.noreply.github.com>
2020-11-02 18:02:30 +0700

    Add Function Return

---
+++ RUPP/FuctionReturn/passedByReference/passedByReference.java
@@ -0,0 +1,26 @@
+// Objects are passed by reference.
+class Test {
+    int a, b;
+
+    Test(int i, int j) {
+       a = i;
+       b = j;
+    }
+    // pass an object. Now, ob.a and ob.b in object
+    // used in the call will be changed.
+    void change(Test ob) {         
+       ob.a = ob.a + ob.b;
+       ob.b = -ob.b;
+    }
+ }
+ class passedByReference {
+    public static void main(String args[]) {
+       Test ob = new Test(15, 20);
+  
+       System.out.println("ob.a and ob.b before call: " +
+                           ob.a + " " + ob.b);
+       ob.change(ob); 
+       System.out.println("ob.a and ob.b after call: " +
+                           ob.a + " " + ob.b);
+    }
+ }
